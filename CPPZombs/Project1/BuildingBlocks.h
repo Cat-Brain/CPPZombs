3bc9612f1b0d211ebc159bc1687ba0d95ee56af5
jordan baumann <0501jordanb@gmail.com>
2022-10-31 10:25:45 -0400

    Functional block

--- CPPZombs/Project1/BuildingBlocks.h
+++ CPPZombs/Project1/BuildingBlocks.h
@@ -16,3 +16,23 @@ public:
 		screen->Draw(pos.x, screenHeight - pos.y - 1, Color(color2.r + t * (color.r - color2.r), color2.g + t * (color.g - color2.g), color2.b + t * (color.b - color2.b), color2.a + t * (color.a - color2.a)));
 	}
 };
+
+class FunctionalBlock : Entity
+{
+public:
+	using Entity::Entity;
+	int ticksPerActivate = 1;
+
+	void Update(olc::PixelGameEngine* screen, vector<Entity*> entities, int frameCount, Inputs inputs) override
+	{
+		if (frameCount % ticksPerActivate == 0)
+			TUpdate(screen, entities, frameCount, inputs);
+
+		Entity::Update(screen, entities, frameCount, inputs);
+	}
+
+	virtual void TUpdate(olc::PixelGameEngine* screen, vector<Entity*> entities, int frameCount, Inputs inputs)
+	{
+
+	}
+};
